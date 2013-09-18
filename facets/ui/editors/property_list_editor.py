2197782ddd1fc11f23e1f5906f8e6da194a0ff29
David C. Morrill <david.morrill@gmail.com>
2013-09-18 12:36:12 -0400

    Extended the PropertyListEditor
    Extended the PropertyListEditor to support more of the
    PropertySheetEditor feature set.

--- facets/ui/editors/property_list_editor.py
+++ facets/ui/editors/property_list_editor.py
@@ -17,12 +17,31 @@ As an alternative to using the simple tuple form described above, instances of
 PropertyListItem can also be used:
 
     PropertyListItem(
-        object = HasFacets_object,
-        facet  = facet,
-        label  = label,
-        editor = editor,
-        mode   = mode
+        object                = Instance( HasFacets ),
+        name                  = Str,
+        label                 = Str,
+        editor                = Instance( EditorFactory ),
+        mode                  = EditMode,
+        formatter             = Callable,
+        image                 = Str,
+        image_alignment       = CellImageAlignment,
+        paint                 = Either( Str, Callable ),
+        theme                 = ATheme,
+        label_image           = Str,
+        label_image_alignment = CellImageAlignment,
+        label_paint           = Either( Str, Callable ),
+        label_theme           = ATheme,
+        label_menu            = Any,
+        tooltip               = Str,
+        is_open               = Bool,
+        show_children         = Bool,
+        show_group            = Bool
     )
+
+    Using an explicit PropertyListItem provides greater flexibility than using
+    a simple tuple, but can be somewhat more verbose. Note that internal to the
+    PropertyListEditor, all items are represented using PropertyListItem
+    instances.
 """
 
 #-------------------------------------------------------------------------------
@@ -34,7 +53,8 @@ PropertyListItem can also be used:
 #-------------------------------------------------------------------------------
 
 from facets.api \
-    import HasFacets, Instance, Str, List, Any, EditorFactory
+    import HasFacets, Instance, Str, List, Any, ATheme, Bool, Callable, \
+           EditorFactory
 
 from facets.core.facet_base \
     import user_name_for
@@ -44,6 +64,9 @@ from facets.ui.editors.property_sheet_editor \
 
 from facets.ui.property_sheet_adapter \
     import PropertySheetAdapter, EditMode
+
+from facets.ui.ui_facets \
+    import CellImageAlignment
 
 #-------------------------------------------------------------------------------
 #  'PropertyListItem' class:
@@ -59,14 +82,60 @@ class PropertyListItem ( HasFacets ):
     # The name of the object facet being edited:
     name = Str
 
-    # The UI label for the facet being edited:
+    # The UI label to use:
     label = Str
 
-    # The Editor used to edit the facet:
+    # The Editor to use:
     editor = Instance( EditorFactory )
 
-    # The editing mode used to edit the facet:
+    # The editing mode to use:
     mode = EditMode
+
+    # The formatting function to use:
+    formatter = Callable
+
+    # The name of the default image to use:
+    image = Str( None )
+
+    # Horizontal image alignment to use for a value cell:
+    image_alignment = CellImageAlignment
+
+    # The custom function to use for rendering a value cell:
+    paint = Any # Either( Str, Callable )
+
+    # The theme to use for rendering a value cell:
+    theme = ATheme
+
+    # The name of the default image to use for a label cell:
+    label_image = Str( None )
+
+    # Horizontal image alignment to use for a label cell:
+    label_image_alignment = CellImageAlignment
+
+    # The custom function to use for rendering a label cell:
+    label_paint = Any # Either( Str, Callable )
+
+    # The theme to use for rendering a label cell:
+    label_theme = ATheme
+
+    # The menu to use for a label cell:
+    label_menu = Any
+
+    # The tooltip to display for the current item:
+    tooltip = Str
+
+    # The initial open/close state for the current (group) item:
+    is_open = Bool( True )
+
+    # Does the current HasFacets, list, tuple or dict item have child items that
+    # should be displayed in the property sheet (True) or not (False)?
+    show_children = Bool( True )
+
+    # If the current HasFacets, list, tuple or dict item has visible child items
+    # (because 'show_children' is True), should the child items be children of
+    # a new group representing the current item (True), or simply be added to
+    # the items for the current group (False)?
+    show_group = Bool( True )
 
     #-- Facet Default Values ---------------------------------------------------
 
@@ -76,6 +145,19 @@ class PropertyListItem ( HasFacets ):
 
     def _editor_default ( self ):
         return self.object.base_facet( self.name ).get_editor()
+
+
+    def _formatter_default ( self ):
+        return self._format_value
+
+    #-- Private Methods --------------------------------------------------------
+
+    def _format_value ( self, value ):
+        """ Returns a formatted version of the specified value using the
+            'string_value' method associated with the items' object facet.
+        """
+        return self.object.base_facet( self.name ).string_value(
+                                                 self.object, self.name, value )
 
 #-------------------------------------------------------------------------------
 #  'PropertyListAdapter' class:
@@ -89,22 +171,14 @@ class PropertyListAdapter ( PropertySheetAdapter ):
     # edited:
     items = List
 
-    # The current PropertyListItem being processed:
-    current_item = Instance( PropertyListItem )
+    # Mapping from ( object, name ) tuples to PropertyListItem's:
+    item_map = Any( {} )
+
 
     #-- Property Implementations -----------------------------------------------
 
     def _get_facets ( self ):
         return [ str( i ) for i in xrange( len( self.items ) ) ]
-
-
-    def get_alias ( self, object, name ):
-        """ Returns the ( object, name ) alias to use in place of the specified
-            object and facet name.
-        """
-        self.current_item = item = self.items[ int( name ) ]
-
-        return ( item.object, item.name )
 
 
     def get_label ( self, object, name ):
@@ -114,18 +188,131 @@ class PropertyListAdapter ( PropertySheetAdapter ):
         return self.items[ int( name ) ].label
 
 
-    def get_editor ( self, object, name ):
+    def get_alias ( self, object, name ):
+        """ Returns the ( object, name ) alias to use in place of the specified
+            object and facet name.
+        """
+        item                    = self.items[ int( name ) ]
+        result                  = ( item.object, item.name )
+        self.item_map[ result ] = item
+
+        return result
+
+
+    def get_editor ( self, *args ):
         """ Returns the editor to use for editing the specified object and
             facet name.
         """
-        return self.current_item.editor
+        return self.item_map[ args ].editor
 
 
-    def get_mode ( self, object, name ):
+    def get_mode ( self, *args ):
         """ Returns the editing mode to use for the specified object and
             facet name.
         """
-        return self.current_item.mode
+        return self.item_map[ args ].mode
+
+
+    def get_formatter ( self, *args ):
+        """ Returns the formatting function to use for the specified object and
+            facet name.
+        """
+        return self.item_map[ args ].formatter
+
+
+    def get_image ( self, *args ):
+        """ Returns the value cell image name to use for displaying the
+            specified object and facet name.
+        """
+        return self.item_map[ args ].image
+
+
+    def get_image_alignment ( self, *args ):
+        """ Returns the value cell image alignment to use for displaying the
+            specified object and facet name.
+        """
+        return self.item_map[ args ].image_alignment
+
+
+    def get_paint ( self, *args ):
+        """ Returns the value cell painter to use for displaying the specified
+            object and facet name.
+        """
+        return self.item_map[ args ].paint
+
+
+    def get_theme ( self, *args ):
+        """ Returns the value cell theme to use for displaying the specified
+            object and facet name.
+        """
+        return self.item_map[ args ].theme
+
+
+    def get_label_image ( self, *args ):
+        """ Returns the label cell image name to use for displaying the
+            specified object and facet name.
+        """
+        return self.item_map[ args ].label_image
+
+
+    def get_label_image_alignment ( self, *args ):
+        """ Returns the label cell image alignment to use for displaying the
+            specified object and facet name.
+        """
+        return self.item_map[ args ].label_image_alignment
+
+
+    def get_label_paint ( self, *args ):
+        """ Returns the label cell painter to use for displaying the specified
+            object and facet name.
+        """
+        return self.item_map[ args ].label_paint
+
+
+    def get_label_theme ( self, *args ):
+        """ Returns the label cell theme to use for displaying the specified
+            object and facet name.
+        """
+        return self.item_map[ args ].label_theme
+
+
+    def get_label_menu ( self, *args ):
+        """ Returns the label cell menu to use for the specified object and
+            facet name.
+        """
+        return self.item_map[ args ].label_menu
+
+
+    def get_tooltip ( self, *args ):
+        """ Returns the tooltip to display for the specified object and facet
+            name.
+        """
+        return self.item_map[ args ].tooltip
+
+
+    def get_is_open ( self, *args ):
+        """ Returns the initial open state to use for the specified object and
+            group name.
+        """
+        return self.item_map[ args ].is_open
+
+
+    def get_show_children ( self, *args ):
+        """ Returns if the HasFacets, list, tuple or dict object returned by
+            getattr(object, name) has child items that should be displayed in
+            the property sheet (True) or not (False).
+        """
+        return self.item_map[ args ].show_children
+
+
+    def get_show_group ( self, *args ):
+        """ Returns if the HasFacets, list, tuple or dict object returned by
+            getattr(object, name) that has visible child items should display
+            those items as children of a new group representing that object
+            (True), or simply be added to the list of items that the object is
+            part of (False).
+        """
+        return self.item_map[ args ].show_group
 
 #-------------------------------------------------------------------------------
 #  '_PropertyListEditor' class:
